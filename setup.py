7de7311e22c8f142cba0235b6c5aa19ea5b66f1e
Stephen Wolff <stephenhwolff@gmail.com>
2018-03-07 15:08:13 -0500

    Restructured file system to match setup.py expectations

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pythonai',
+      version='0.1',
+      description='Python AI playground and testing',
+      url='http://github.com/wolffereast/pythonai',
+      author='Stephen Wolff',
+      author_email='stephenhwolff@gmail.com',
+      license='MIT',
+      packages=['pythonai'],
+      install_requires=[
+          'mock',
+      ])
